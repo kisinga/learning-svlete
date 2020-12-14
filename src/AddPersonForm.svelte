01066b3699093a88d1a6395ca8d8b82beb4be04c
kisinga <kamana@kisinga.family>
2020-12-14 10:16:04 +0300

    binding: the differences are starting to show

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,8 +2,12 @@
     let name;
     let belt;
     let age;
+    // let fighting = false;
+    // let sneaking = false;
+    // let running = false;
+    let skills = [];
     const handleSubmit = () => {
-        console.log(name, belt, age);
+        console.log(name, belt, age, skills);
     };
 </script>
 
@@ -12,7 +16,20 @@
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name} />
-    <input type="text" placeholder="belt" bind:value={belt} />
+    <!-- <input type="text" placeholder="belt" bind:value={belt} /> -->
     <input type="number" placeholder="age" bind:value={age} />
+    <label>Skills:</label>
+    <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+    <input type="checkbox" bind:group={skills} value="sneaking" />sneaking<br />
+    <input type="checkbox" bind:group={skills} value="running" />running<br />
+    <label>Belt colour:</label>
+    <select name="beltcolour" id="" bind:value={belt}>
+        <option value="black">black</option>
+        <option value="blue">blue</option>
+        <option value="red">red</option>
+        <option value="green">green</option>
+        <option value="yellow">yellow</option>
+        <option value="white">white</option>
+    </select>
     <button type="submit">Add person</button>
 </form>
