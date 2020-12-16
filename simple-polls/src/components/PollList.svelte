a60c2867614ebfe3d69108c39538e63cc69bd05c
kisinga <kamana@kisinga.family>
2020-12-16 12:07:41 +0300

    amsll animations and transitions

--- simple-polls/src/components/PollList.svelte
+++ simple-polls/src/components/PollList.svelte
@@ -1,6 +1,8 @@
 <script type="ts">
     import PollDetails from "./PollDetails.svelte";
     import PollStore from "../stores/PollStore";
+    import { fade, slide, scale } from "svelte/transition";
+    import { flip } from "svelte/animate";
 </script>
 
 <style>
@@ -13,7 +15,7 @@
 
 <div class="poll-list">
     {#each $PollStore as poll (poll.id)}
-        <div>
+        <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
             <PollDetails {poll} />
         </div>
     {/each}
